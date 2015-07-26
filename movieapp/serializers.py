9b10cce07c1367dd20f89b87ae43a640b8c75d2b
Rush <rushidadhaniya@gmail.com>
2015-07-26 23:33:25 +0530

    Added authentication support and permissions

--- movieapp/serializers.py
+++ movieapp/serializers.py
@@ -1,9 +1,19 @@
-from movieapp.models import Movie
 from rest_framework import serializers
-from django.forms import widgets
+from django.contrib.auth.models import User
+from movieapp.models import Movie
 
 
 class MovieSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
     class Meta:
         model = Movie
-        fields = ('id', 'name', 'director', 'popularity', 'movie_mania_score', 'duration', 'genre')
+        fields = ('id', 'name', 'director', 'popularity', 'movie_mania_score', 'duration', 'genre', 'owner')
+
+
+class UserSerializers(serializers.ModelSerializer):
+    movies = serializers.PrimaryKeyRelatedField(many=True, queryset=Movie.objects.all())
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'movies')
--- movieapp/views.py
+++ movieapp/views.py
@@ -1,13 +1,34 @@
 from rest_framework import generics
 from movieapp.models import Movie
 from movieapp.serializers import MovieSerializer
+from django.contrib.auth.models import User
+from movieapp.serializers import UserSerializers
+from rest_framework import permissions
+from movieapp.permissions import IsOwnerOrReadOnly
 
 
 class MovieList(generics.ListCreateAPIView):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
 
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
+
 
 class MoviewDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializers
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializers
