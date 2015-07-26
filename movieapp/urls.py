35ac4a41a3e6629e5cb1a79db2de7cd9d14eb550
Rush <rushidadhaniya@gmail.com>
2015-07-26 20:53:40 +0530

    Refactored code to use django generic API view

--- movieapp/urls.py
+++ movieapp/urls.py
@@ -3,8 +3,8 @@ from movieapp import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    url(r'^movies/$', views.movie_list),
-    url(r'^movies/(?P<pk>[0-9]+)/$', views.movie_details),
+    url(r'^movies/$', views.MovieList.as_view()),
+    url(r'^movies/(?P<pk>[0-9]+)/$', views.MoviewDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- movieapp/views.py
+++ movieapp/views.py
@@ -1,45 +1,13 @@
-from rest_framework.parsers import JSONParser
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import generics
 from movieapp.models import Movie
 from movieapp.serializers import MovieSerializer
 
 
-@api_view(['GET', 'POST'])
-def movie_list(request, format=None):
-    if request.method == 'GET':
-        movies = Movie.objects.all()
-        serializer = MovieSerializer(movies, many=True)
-        return Response(serializer.data)
-
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = MovieSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class MovieList(generics.ListCreateAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def movie_details(request, pk, format=None):
-    try:
-        movie = Movie.objects.get(pk=pk)
-    except Movie.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = MovieSerializer(movie)
-        return Response(serializer.data)
-
-    elif request.method == 'PUT':
-        serializer = MovieSerializer(movie, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == 'DELETE':
-        movie.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class MoviewDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
