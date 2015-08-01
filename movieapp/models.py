9d3f807e9279617cdfd10c618e3c98e077d69d9c
Rush <rushidadhaniya@gmail.com>
2015-08-01 18:32:55 +0530

    Added many to many related model for genre

--- movieapp/models.py
+++ movieapp/models.py
@@ -1,12 +1,28 @@
 from django.db import models
 
 
+class Genre(models.Model):
+    genre = models.CharField(max_length=100, default='')
+
+    def __unicode__(self):
+        return self.genre
+
+    class Meta:
+        ordering = ('genre',)
+
+
 class Movie(models.Model):
     created = models.DateTimeField(auto_now_add=True)
-    name = models.CharField(max_length=100, blank=False)
+    movie_name = models.CharField(max_length=100, blank=False)
     director = models.CharField(max_length=100)
     popularity = models.FloatField()
     movie_mania_score = models.FloatField()
     duration = models.CharField(max_length=20)
-    genre = models.CharField(max_length=200)
-    owner = models.ForeignKey('auth.User', related_name='movies')
+    genre = models.ManyToManyField('Genre', default='')
+    owner = models.ForeignKey('auth.User', related_name='movies', default=1)
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('movie_name',)
